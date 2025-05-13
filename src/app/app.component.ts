ef4513cfbb0bba5b52af2294d6643f35ebe56e09
Axel <axew@ciencias.unam.mx>
2025-05-13 16:17:40 -0600

    :sparkles: NgFor sesion 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, DirectivaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido al curso de Angular';
+  curso: string = 'Angular 16';
+  profesor: string = 'Andres Guzman';
 }
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+  styleUrls: ['./directiva.component.css'],
+  imports: [CommonModule]
+})
+export class DirectivaComponent {
+  listaCurso : string[] = ['TypeScript', 'JavaScript', 'Java', 'C#', 'PHP'];
+
+  constructor() {}
+
+}
